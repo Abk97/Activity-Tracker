3bbe8c0cd73fb0b19e6d42b4ddf7f999cc9dead3
AbK97 <abk013@ucsd.edu>
2020-07-05 22:59:58 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-//require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,8 +19,8 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
+//const exercisesRouter = require('./routes/exercises');
+//const usersRouter = require('./routes/users');
 
 //app.use('/exercises', exercisesRouter);
 //app.use('/users', usersRouter);
